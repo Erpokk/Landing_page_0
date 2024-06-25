b656327131709bf23d78f82f7c698bffbcdb308e
Maksym Adamneko <maimav56@gmail.com>
2024-06-25 12:41:30 +0300

    Header rdy

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ burgerButton.addEventListener('click', function() {
                 mobileOverlay.classList.remove('is-open');
             });
             
+
+function removeSmallTextClass() {
+    if (window.innerWidth >= 1158) {
+        document.querySelectorAll('.address-item').forEach(function (element) {
+            element.classList.remove('small-text');
+        });
+    }
+    else {
+        document.querySelectorAll('.address-item').forEach(function (element) {
+            element.classList.add('small-text');
+        });
+    }
+}
+
+removeSmallTextClass();
+
+window.addEventListener('resize', removeSmallTextClass);
