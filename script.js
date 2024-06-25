c98278bcec6602fddfc18c371ffba3d7b8221544
Maksym Adamneko <maimav56@gmail.com>
2024-06-25 11:59:41 +0300

    Start from beginig

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const burgerButton = document.getElementById('burgerButton');
 const mobileOverlay = document.getElementById('mobileOverlay');
 const closeButton = document.getElementById('closeButton');
+const addBar = document.getElementById('addBar');
+
 
 
 burgerButton.addEventListener('click', function() {
@@ -10,19 +12,4 @@ burgerButton.addEventListener('click', function() {
  closeButton.addEventListener('click', function() {
                 mobileOverlay.classList.remove('is-open');
             });
-
-const navList = document.getElementById('nav-list');
-const address = document.getElementById('address');
             
-function checkWindowSize() {
-    if (window.matchMedia('(max-width: 319px)').matches) {
-        navList.classList.add('visually-hidden');
-        address.classList.add('visually-hidden');
-    } else {
-        navList.classList.remove('visually-hidden');
-        address.classList.remove('visually-hidden');
-    }
-}
-
-window.addEventListener('resize', checkWindowSize);
-window.addEventListener('DOMContentLoaded', checkWindowSize);
